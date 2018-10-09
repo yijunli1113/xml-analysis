a3d3d1ee8a0886088015fe6730a1db5296782b0d
宜駿 李 <1105105225@office365.kuas.edu.tw>
2018-10-10 00:31:46 +0800

    modify

---
+++ OpenData/OpenData/Models/OpenData.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace OpenData
+{
+    class OpenData
+    {
+        public string 資料集名稱 { get; set; }
+        public string 授權方式 { get; set; }
+        public string 資料集描述 { get; set; }
+        public string 資料集提供機關聯絡人 { get; set; }
+    }
+    
+
+}
